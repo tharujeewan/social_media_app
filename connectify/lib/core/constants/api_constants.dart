890b3fb39436d7362c06b51b7a53faa13eab5aff
Jeewan Chaudhary <jeewantharu234@gmail.com>
2026-06-29 14:54:25 +0545

    ready to deploy in ec2

--- connectify/lib/core/constants/api_constants.dart
+++ connectify/lib/core/constants/api_constants.dart
@@ -2,11 +2,13 @@ class ApiConstants {
   ApiConstants._();
 
   // ── Base URL ──────────────────────────────────────────────────────────
-  static const String baseUrl = 'https://social-media-app-78yx.onrender.com/api/v1'; // Production
+  // Switch between local and production by commenting/uncommenting:
 
-  // static const String baseUrl = 'http://192.168.1.64:3000/api/v1'; // Physical device (Wi-Fi)
-  // static const String baseUrl = 'http://10.0.2.2:3000/api/v1';     // Android emulator
-  // static const String baseUrl = 'http://localhost:3000/api/v1';     // iOS simulator
+  static const String baseUrl = 'http://192.168.1.68:3000/api'; // Physical device (Wi-Fi)
+
+  // static const String baseUrl = 'http://10.0.2.2:3000/api';     // Android emulator
+  // static const String baseUrl = 'http://localhost:3000/api';     // iOS simulator
+  // static const String baseUrl = 'https://social-media-app-78yx.onrender.com/api/v1'; // Production
 
   // ── Auth Endpoints ────────────────────────────────────────────────────
   static const String login = '/auth/login';
